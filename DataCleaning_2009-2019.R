a542feefe41580be23c522db698780e0011c0f0e
jannekehrl <jhrl@uw.edu>
2025-10-17 14:34:35 +0200

    created file to clean 2009-2019 data. For 2020 onwards, we have a new process where the files are cleaned after data entry and sorting, and before being deposited with github.

---
+++ DataCleaning_2009-2019.R
@@ -0,0 +1,4 @@
+##########################################################
+### script to clean data files from 2009-2019          ###
+### also used to generate StandTrapInventory Files     ###
+#########################################################
