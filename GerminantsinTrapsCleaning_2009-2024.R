89e29627bc0f8ec111cdb50f75d646f59c7ee339
jannekehrl <jhrl@uw.edu>
2025-10-18 21:59:58 +0200

    created new file for cleaning germinants in traps

---
+++ GerminantsinTrapsCleaning_2009-2024.R
@@ -0,0 +1,9 @@
+####################################################################
+### script to clean germinants in trap files from 2009-2024      ###
+### also used to generate 2009-2019 StandTrapInventory Files     ###
+### created by Janneke Hille Ris Lambers, 17.10.2025             ###
+####################################################################
+
+############
+##Germinants in traps data files
+##Note - 2009 - 2017 data in one format, 2018 onwards in another
