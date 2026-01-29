ab97a77859617cc427404e96d6a515e90fc1f2e3
eleoperret <153828080+eleoperret@users.noreply.github.com>
2026-01-29 23:15:22 +0100

    Progress from today. Still did not manage something good. I will try now to work on a hurdle model.

--- StartingWithStan.R
+++ StartingWithStan.R
@@ -65,6 +65,7 @@ stand_elevations <- c(
   "SUNR" = 1800
 )
 
+
 #Adding the elevation of the stands to the results
 #Creating a dataframe with the stand data
 elev_df <- data.frame(
@@ -110,16 +111,19 @@ ggplot(total_stand_psme,
   labs(x = "Stand (ordered by elevation)",
        y = "Total viable seeds") +
   theme(axis.text.x = element_text(angle = 90))
-#much more what I would expect. I will use this specie for the moment. 
+
+#I can see here that some stand have 0 seed production in 15 years (very unlikely that it will change) except for SPRY and AR07 (but to be honest I'm a bit suprised; in total it is 2 seeds found for the AR07 and 3 for the SPRY over the last years; what to do with that. I honestly cannot refute the misidentification because there are no adult tree anywhere close to this area so something to think about)
 
 
-
-# Modelling production/elevation with Stan --------------------------------
+# Modelling production/elevation with Stan  --------------------------------
 str(psme_data)
 
-#Key points: 
+#Key points for myself: 
 #Everything that has to do with definin the distribution, the prior, the parameter, the likelihood is in the .stan file
 #Anything with data prep, scaling, plotting, extracting results is in R. 
+
+
+
 
 ###STEP 1 : Understand the data
 #Attaching my elevation data to my dataset.
@@ -147,19 +151,19 @@ total_obs <- nrow(psme_data)
 # Fraction of zeros
 fraction_zeros <- num_zeros / total_obs
 fraction_zeros #0.5243517
-#52% of my observations are zero seeds (which is a lot of 0'S)
+#52% of my observations are zero seeds (which is a lot of 0'S), probably coming a lot from those high elevations that have 0 seed prduction
 
-
+##Thinking about the elevation now
 #I need to scale my elevation for STAN stability? why? because it keeps the predictor around 0 and the SD= 1. Large numbers can cause instability. 
 #Is instability bad?
 psme_data$elev_sc <- scale(psme_data$elevation)[,1]
-#and build a stan list where I define my N, y etc...
-stan_data <- list(
-  N = nrow(psme_data),
-  y = psme_data$total_viable_sds,
-  elev = psme_data$elev_sc
-)
 
+length(unique(psme_data$stand))
+length(unique(psme_data$elevation))
+#Some stands share the same elevation
+#Not a problem for this example but I think that it could be a problem for the bigger model as there are some stands with similar elevation yes but with diffirent species composition and slope and orientation (N/S) so I think if we want to use the stand as a proxy for elevation or climatic/environemental conditions it would have to be taken into account
+
+#Thinking more about my data
 #Density plot of my data
 #This shows me where my data is concentrated. I can see that most of it is close to 0. 
 #X-axis : Each point is one observation (year/stand/trap)
@@ -182,38 +186,125 @@ p2 <- ggplot(psme_data, aes(x = total_viable_sds)) +
   ggtitle("Density")
 p1 + p2
 #So seed count is relatively low in general. 
-#Interpretation: Seeds are usually not high but occasionally some high years or high sites which are shown with the end of the curve. So this could be masting or some sites with higher production?
+#Interpretation: Seeds are usually not high but occasionally some high years or because of the fact that some sites never produce seeds which are shown with the end of the curve. So this could be masting or an artefact of the discrpency between sites
 
-#Another way of visualizing my data to see the distribution of the seed productio nbased on each stand
+#Another way of visualizing my data to see the distribution of the seed productio nbased on each stand *Similar as above but here I can see the density distribution for each stand
 ggplot(psme_data, aes(x = total_viable_sds)) +
   geom_density() +
   facet_wrap(~ stand)
 
-summary(psme_data$elev_sc)
-range(psme_data$elev_sc)
-#This can help me define the range for my beta prior
+#And year
+ggplot(psme_data, aes(x = total_viable_sds)) +
+  geom_density() +
+  facet_wrap(~ year)
+#Interesting as we can see that some years have very low seed production and others quite high, which correspons to the literature. I think that it is really from what I read where PSME have ~7 years cycle so there is natural variation maybe pushed by other species but the very high years are around this 7 year cycle.
+ggplot(psme_data,
+       aes(x = year, y = total_viable_sds)) +
+  geom_col() +
+  theme_bw() +
+  labs(x = "Year",
+       y = "Total viable seeds") +
+  theme(axis.text.x = element_text(angle = 90))
+
+
+##*Before going to the next step there a few things to consider. Some years have low and high seed production I think this should not be an issue but should I but this as a prior?
+##*There are stands where there is no seed production so this is quite tricky because it makes it harder to model or to have a proper model; at least with what I tried
 
 
 
 
 ###STEP 2: Choosing the likelihood distribution (the model I will use)
 #My options
-# a) I thoutgh that I could try first with a *poisson distribution* as it is count data but as the poisson distribution assumes that the mean = var (see line 126-127) I think it is not appropriate. 
-# b) I could try now a *negative binomial* (adding a dispersion parameter so that my variance > mean which is useful for overdispersion of the seed counts.) But I have an excess of 0'S (see line 138) and this my underestimate the probabiliy of 0's
-# c) I can also try a *zero-inflated Negative Binomial* if I have a lot of 0's which is the case. I think it is the best option as it can handle overdispersion and many 0's 
+# a) I thoutgh that I could try first with a *poisson distribution* as it is count data but as the poisson distribution assumes that the mean = var (see line 137) I think it is not appropriate because Var>>Mean. 
+# b) I could try a *negative binomial* (adding a dispersion parameter so that my variance > mean which is useful for overdispersion of the seed counts.) But I have an excess of 0'S and this my underestimate the probabiliy of 0's
+# c) I think the best option is to try a *zero-inflated Negative Binomial* if I have a lot of 0's which is the case. I think it is the best option as it can handle overdispersion and many 0's 
 
 #Model justification : why log? Seed counts are non-negative integers and doing exp() ensure that alpha + beta * elevation is always =or > than 0. but the multiplicative effect (from beta) becomes additive so it is easier to interpret and alows for a stable model. It stabilizes the variance for overdispesed count data
 
 
-###In .stan file now
-
 ###STEP 3: Specify model structure and choosing priors.  Creating my STAN file that I will name (psme_poisson.stan) where I choose my model structure and my priors (the set of values that are possible) and where I can write my prior predictive check (~are the model assumptions correct~shapiro test)
+
+# Justification on how I chose my priors.
+# model : log(expected number of seeds)= alpha + beta * elevation
+
+#Alpha. (the intercept). 
+#It is the expected log seed count when elevation is 0. The baseline seed production 
+# alpha~normal(a,b). First the mean of the normal distribution and then the standard deviation(spread of plausible alpha values)
+#As my mean seed is around 4 and alpha is on a log scale. So log(4)~1,4. Then if you look at the density plot my values are mostly around 0-17 seeds with some case of extreme seed production (which is handled by my phi-- overdispersion), so then I should select for a SD that would be within this range of most datapoint which one would be good so I would have alpha ranging between 0,4 and 2,4 which would meanbetween 1,5 and 11 seeds (????maybe I could use another one like 1.1 or 1.3 ????). The SD here has to reflect the plausible variation on the log scale not the extreme outliers that will be handled by phi. 
+
+#Beta. (the slope)
+#beta is the slope of elevation and we assume always at the beginning that there is no effect of elevation.(Mean = 0 so no effect, and SD, plausible range)
+# It is how stronlgy seed production changes with elevation if beta is >0 then the seed production increase otherwise descrease. It is the strength of my effect
+#What is my effect? It is the strength at with my response changes when a predictor changes by one unit. 
+summary(psme_data$elev_sc)
+range(psme_data$elev_sc) #-1.573070  2.021847
+#This can help me define the range for my beta prior
+#As I know that my seed produciton decrease with elevation 
+lm_fit <- lm(log(total_viable_sds + 1) ~ elev_sc, data = psme_data)
+summary(lm_fit)
+#As I did not check for the validity of my lm but just used it to have a sort of reference about the decrease of seed production, I will use a negative slope of 0,5 and a SD of 0,5 (maybe too large?).Shoudl I define this more clearly? Because it doesn't seem linear so using an lm is for sure wrong. I don't know.. 
+
+
+#Phi. (the overdispersion). 
+#phi is the dispersion parameter that allows my variance to be larger than my mean. in a NB, Var(y)= mean (y) + mean(y)^2/phi. 
+#Type of distribution for the phi parameter (dispersion)
+p2
+curve(dexp(x,1),0,5)        # exponential
+curve(dlnorm(x,-1,0.7),0,5) # lognormal
+curve(dgamma(x,2,1),0,5)    # gamma
+#Based on the curves, it would make the most sense to use a exp. or maybe lognorm 
+#exponential use a rate ^ and I can use a moderate one (1)or a slighty wider prior but than the rate is lower (0,5). This would account for the higly variable counts. 
+#However, I tried and it doesn't work well so I will  phi ~ gamma(2, 2)
+#It still doesn't work so I could potentially remove the 0 inflation. --> Did not work
+#I tried with lognormal(-1, 0.5)
+
+#Theta. (the zero inflated)
+#theta is the probability of a structural zero. It accounts for the excta 0's. and is bounded between 0 an 1. 
+#Because I know that I have 50% of my data that is 0, this means that I have a and b that are symmetric so using (5,5) means that I have 0,5 for my theta prior which reflects my data.
+#Type of distribution for the theta parameter (probability of 0's)
+curve(dbeta(x,1,1), 0,1)   # uniform, mean 0.5, max variance
+curve(dbeta(x,2,2), 0,1)   # symmetric, mean 0.5, less spread
+curve(dbeta(x,5,5), 0,1)   # symmetric, mean 0.5, tighter
+curve(dbeta(x,10,5), 0,1)  # skewed, mean 0.667
+#I tried that but doesn't work because my 0's are not distributed equally. I have the high sites with 0 seed prodcution so I decided to create a parameter that depends on the elevation where I have gamma0 (probability of 0) and gamma1 (effect of elevation) that control the mean and the slope and because I know that there are more 0's with elevations. Gamma0 is the baselline  and the gamma 1 is the slope 
+#theta is now : inv_log(gamma0 + gamma1 *elev), where the inv_logit turns numbers nto a probability between 0 and 1 
+#So gamma0 ~normal (-2,1) where inv_logit(-2)~0,12 so low probability of 0's at low elevation and SD 1 so allows uncertainity
+# Gamma 1~normal(2,1) which 2 indicating a steep increase in 0's (maybe this can be changes too, into 1 see plot afterwards) and again 1 for allowing uncertainity 
+#Checking it visually
+gamma0 <- -2
+gamma1 <- 2
+elev <- seq(-2, 2, length.out=100)
+theta <- plogis(gamma0 + gamma1*elev)
+plot(elev, theta, type='l', ylab='θ', xlab='Elevation', main='Prior θ curve')
+#looking at the 0'S from my data
+zero_curve <- psme_data %>%
+  group_by(elev_sc) %>%
+  summarise(
+    n = n(),
+    n_zero = sum(total_viable_sds == 0),
+    prop_zero = mean(total_viable_sds == 0)
+  )
+ggplot(zero_curve, aes(x = elev_sc, y = prop_zero)) +
+  geom_point(size=3, color="red") +      
+  geom_line(color="blue", size=1) + 
+  geom_smooth(method="glm", method.args = list(family = "binomial"),
+              se=FALSE, color="darkgreen", linetype="dashed") +
+  ylim(0,1) +
+  xlab("Scaled Elevation") +
+  ylab("Proportion of zeros") +
+  ggtitle("Observed proportion of zero seed counts by elevation") +
+  theme_minimal()
+#Looks quite similar maybe?
+
+
+
 
 
 ###STEP 4 : Prior predictive check. 
 #If simulated count are way to high or all zeros than my priors need adjusting
 #Once my prior predictive check looks good I can fit my model with the real data 
 
+#First I need to create a list for the priors that I call ppc
 stan_data_ppc <- list(
   N = nrow(psme_data),
   y = rep(0, nrow(psme_data)),  # dummy values instead of the real ones, creates a vecto of 0 to satisfy STAN data requirement. 
@@ -223,16 +314,20 @@ stan_data_ppc <- list(
 #This is getting what I wrote in the .rstan file
 # Translating my .stan code into C++ code and then compiling the C++ into a standalone executable and then saving it so it can run the MCMC sampling. 
 mod <- cmdstan_model("psme_zinb.stan")
+#alternative without zero inlfation
+#mod <- cmdstan_model("psme_nb.stan")
 
 fit_ppc <- mod$sample(
   data = stan_data_ppc,
   chains = 4,
   parallel_chains = 4,
-  iter_warmup = 500,
+  iter_warmup = 500, #can be modified depending on how many times I want to do
   iter_sampling = 500
 )
 
-print(fit_ppc, variables = c("alpha", "beta", "phi", "theta"))
+print(fit_ppc, variables = c("alpha", "beta", "phi", "gamma0", "gamma1"))
+#print(fit_ppc, variables = c("alpha", "beta", "phi"))
+
 
 #How to interpet this : 
 #mad is the median absolute deviation of posterior draws. It measrures spread and sometimes reported as an alternative to SD. Is it important? 
@@ -242,14 +337,13 @@ print(fit_ppc, variables = c("alpha", "beta", "phi", "theta"))
 #ESS-tail: effective sample for the tail of the posterior, important for credible intervals (especially if the distribution is skewdwd). Low ess tail is uncertainity in the extremes. 
 
 
-
+#Prior predictive check.
+#I have to first extract y_sim (from the simulated data) and then convert it to a 2D matrix (iterations*observations) because Stan give (chains*draws*N). I then select 200 draws for plotting and then plot to see it the priors predicts reasonabily 
 # Extract prior predictive draws
 y_sim <- fit_ppc$draws("y_sim")
-#Plot prior predictive check
-# Extract y_sim as 2D because the ysim is actually 3D I need to reduce: iterations x observations (combine all chains automatically)
+# Extract y_sim as 2D 
 yrep <- as_draws_matrix(fit_ppc$draws("y_sim"))
-# 50 draws for plotting
-yrep <- yrep[1:200, ]
+yrep <- yrep[1:200, ] # 200 draws for plotting
 # Plot density overlay
 ppc_dens_overlay(y = psme_data$total_viable_sds, yrep = yrep) +
   ggtitle("Prior Predictive Check: PSME seed counts") +
@@ -261,7 +355,13 @@ ppc_dens_overlay(y = psme_data$total_viable_sds, yrep = yrep) +
 ###STEP 5: Fitting the model to the real data
 #Stan will estimate a posterior distribution
 #Stan uses both priors an dlikelihood from real data. 
-#the y_sim in generated quantiles (.stan file) is now a posterior predictive draw
+
+#list for posterior draws
+stan_data <- list(
+  N = nrow(psme_data),
+  y = psme_data$total_viable_sds,
+  elev = psme_data$elev_sc
+)
 
 fit <- mod$sample(
   data = stan_data,   # this includes real y
@@ -271,13 +371,15 @@ fit <- mod$sample(
   iter_sampling = 1000
 )
 
+#Model output
+fit$summary()
+fit$cmdstan_diagnose()
+#Not good... still too many 0's
 
 ###STEP 6 : Posterior predictive check. 
 # ~ similar to residual diagnostic pots 
 y_post <- fit$draws("y_sim")  # posterior predictive
-
 ypost <- as_draws_matrix(fit$draws("y_sim"))#same as before with reducing the matrix size
-
 ypost_plot <- ypost[1:200, ] #reducing the layout 
 
 ppc_dens_overlay(
@@ -298,103 +400,39 @@ summary(zero_fracs)
 hist(zero_fracs, breaks = 20, xlim= c(0,1))
 abline(v = mean(psme_data$total_viable_sds == 0), col = "red", lwd = 2)
 
-#Model output
-fit$summary()
-fit$cmdstan_diagnose()
+
+
+
+# Hurdle-model ------------------------------------------------------------
+#As the zinb did not really work and I'm not sure if it comes from me or from the fact that this is not a good model, I will try the hurdle model now. 
+
+#Hurdle model (zero vs non-zero). Does the site produce any seeds at all? if it produces seeds how many. 
+#Difference with zinb, is that in the zinb the zeros can come from two different sources whereas the hurdle only from the hurdle bever from the model. 
+
+
+y <- psme_data$total_viable_sds
+elev <- psme_data$elev_sc
+
+# Indicator: 1 = produces seeds, 0 = zero
+is_pos <- as.integer(y > 0)
+
+# Positive counts only
+y_pos <- y[y > 0]
+elev_pos <- elev[y > 0]
+
+stan_data <- list(
+  N = length(y),
+  N_pos = length(y_pos),
+  y = y,
+  y_pos = y_pos,
+  elev = elev,
+  elev_pos = elev_pos
+)
 
 
 
 
 
-#Old Stan likelihood function 
-
-# for (n in 1:N) {
-#   if (y[n] == 0) {
-#     target += log_mix(theta,
-#                       bernoulli_lpmf(1 | 1),
-#                       neg_binomial_2_log_lpmf(y[n] | alpha + beta * elev[n], phi)
-#     );
-#   } else {
-#     y[n] ~ neg_binomial_2_log(alpha + beta * elev[n], phi);
-#   }
-# }
 
 
-#To figure out what distribution I can use for my priors 
-
-#Type of distribution for the theta parameter (probability of 0's)
-curve(dbeta(x,1,1), 0,1)   # uniform, mean 0.5, max variance
-curve(dbeta(x,2,2), 0,1)   # symmetric, mean 0.5, less spread
-curve(dbeta(x,5,5), 0,1)   # symmetric, mean 0.5, tighter
-curve(dbeta(x,10,5), 0,1)  # skewed, mean 0.667
-
-#Type of distribution for the phi parameter (dispersion)
-curve(dexp(x,1),0,5)        # exponential
-curve(dlnorm(x,-1,0.7),0,5) # lognormal
-curve(dgamma(x,2,1),0,5)    # gamma
-
-
-#A prior's check
-
-alpha_draws <- rnorm(1000, mean = 1.4, sd = 1)  # log mean
-mu_draws <- exp(alpha_draws)
-summary(mu_draws)  # check range
-
-theta_draws <- rbeta(1000, 2, 2)
-hist(theta_draws)  # mostly between 0.1 and 0.9
-
-alpha <- rnorm(1000, 1.4, 1)
-beta  <- rnorm(1000, 0, 0.5)
-phi   <- rgamma(1000, 2, 0.5)
-theta <- rbeta(1000, 2, 2)
-
-summary(phi)   # gamma(2,0.5) → mean 4
-summary(beta)  # mean 0, SD 0.5
-
-
-#Simulationg priors
-set.seed(123)
-
-n_draws <- 1000      # number of prior draws
-n_obs   <- 100       # number of observations to simulate
-elev    <- runif(n_obs, -2, 2)  # scaled elevation
-
-# Draw parameters from priors
-alpha <- rnorm(n_draws, 1.5, 1)
-beta  <- rnorm(n_draws, 0, 0.5)
-phi   <- rgamma(n_draws, 2, 0.5)   # NB overdispersion
-theta <- rbeta(n_draws, 2, 2)      # probability of structural zero
-
-#Simulating from the negative binomial ditstribution
-y_sim <- matrix(NA, nrow = n_draws, ncol = n_obs)
-
-for(i in 1:n_draws){
-  mu <- exp(alpha[i] + beta[i] * elev)  # NB mean for each obs
-  for(j in 1:n_obs){
-    if(runif(1) < theta[i]){
-      y_sim[i,j] <- 0                 # structural zero
-    } else {
-      # NB: rnbinom parametrized with size=phi, mean=mu
-      y_sim[i,j] <- rnbinom(1, size = phi[i], mu = mu[j])
-    }
-  }
-}
-
-#Checking for the 0's
-frac_zero <- apply(y_sim, 1, function(x) mean(x==0))
-summary(frac_zero)
-hist(frac_zero, breaks = 20, col = "grey",
-     main = "Fraction of zeros from prior predictive draws",
-     xlab = "Fraction of zeros")
-
-# Add observed fraction of zeros for reference
-observed_frac_zero <- 0.524  # from your data
-abline(v = observed_frac_zero, col = "red", lwd = 2)
-
-#Checking distribution of simulated seeds
-y_all <- as.vector(y_sim)
-
-hist(y_all, breaks = 50, col = "lightblue",
-     main = "Prior predictive distribution of seeds",
-     xlab = "Simulated seed counts")
 
--- psme_zinb.stan
+++ psme_zinb.stan
@@ -1,4 +1,5 @@
-//this is a comment on STAN
+//Stan script for my zero-inflated negative binomial model.
+
 data {
   int<lower=1> N;// number of observations
   vector[N] elev;// predictor :scaled elaation
@@ -13,30 +14,42 @@ parameters {
   real alpha;// intercept for log(count) part
   real beta;// slope of elevation
   real<lower=0> phi;// NB dispersion
-  real<lower=0, upper=1> theta; // probability of structural zero
+ // real<lower=0, upper=1> theta; // probability of structural zero
+ real gamma0; //intercept 
+ real gamma1; //slope
 }
 
+transformed parameters {
+  vector[N] theta_n;
+  for (n in 1:N)
+  theta_n[n]= inv_logit(gamma0 + gamma1 *elev[n]);//Structural zeros depending on elevation
+}
 //This block is what STAN estimates
 //real is a real number
-//This could also be an interger, a numeric vector or a matrix and I can also constrain it.
-//Alpha. There is a unknown quantity or quantities that I want to estimate.It is the expected log seed count when elevation is 0. The baseline seed production 
-//Why log? Because the values cannot be negative with a poisson. 
-//Beta is the slope. How stronlgy seed production changes with elevation if beta is >0 then the seed production increase otherwise descrease. It is the strength of my effect
-//What is my effect? It is the strength at with my response changes when a predictor changes by one unit. 
+
+
+//This is the model block. THe assumption and the learning from my data.
+//It defines my bayesian model 
+
+//liklihood is basically my model 
+// I loop over each observation in the dataset. and then I need to consider if the 0 is a structural 0 (an observation that is always 0) or a sampling zero (a zero that come from the negative binomial distribution by chance)
+//theta is the mixing properties for structural zeros. and log mix comupter log(p*exp(log_f1) + (1-p)*exp(log_f2)) to see if the zero is a structural or if it comes from the negative binomial
+
 
 model {
   // Priors
-  alpha ~ normal(1.5, 1); 
-  beta ~ normal(0, 0.5); 
-  phi ~ gamma(2,0.5);
-  theta ~ beta(2, 2);
+  alpha ~ normal(1.5, 0.5); 
+  beta ~ normal(-0.5, 0.5); 
+  //phi ~ gamma(2,2);
+  phi~lognormal(-1, 0.5);
+  gamma0 ~ normal(-2,1);
+  gamma1 ~normal(2,1);
 
   // Likelihood
   for (n in 1:N) {
    if (y[n] == 0) {
-     target += log_mix(theta,
-                       bernoulli_lpmf(1 | 1),
-                       neg_binomial_2_log_lpmf(y[n] | alpha + beta * elev[n], phi)
+     target += log_mix(theta_n[n],0,//log_prob for structural zero
+     neg_binomial_2_log_lpmf(y[n] | alpha + beta * elev[n], phi)
      );
    } else {
      y[n] ~ neg_binomial_2_log(alpha + beta * elev[n], phi);
@@ -45,34 +58,20 @@ model {
   
 }
 
-//last changes 28.01.26 : tried the gamma but looks worse than the exponential and changed the alpha to 1.5 so maybe try again next time. 
-
-//What to try : Alpha 1.4,1/Beta 0,0.5 or (0,1)/theta beta(5,5)or (10,10) or (20,1) or (2,2)/ phi exponential (1) or lognnormal(-1,0.7) or gamma(2,1)
-
-//Justification on how I chose my priors.
-//model : log(expected number of seeds)= alpha + beta * elevation
-//alpha. As my mean seed is around 4 so if I do log(4) which is 1,4. Then if you look at the density plot my values are mostly around 0-17 seeds with some case of extreme seed production (which is handled by my phi-- overdispersion), so then I should select for a SD that would be within this range of most datapoint which one would be good so I would have alpha ranging between 0,4 and 2,4 which would meanbetween 1,5 and 11 seeds (????maybe I could use another one like 1.1 or 1.3 ????). The SD here has to reflect the plausible variation on the log scale not the extreme outliers that will be handled by phi. 
-//theta. Because I know that I have 50% of my data that is 0, this means that I have a and b that are symmetric so using (5,5) means that I have 0,5 for my theta prior which reflects my data. 
 
 
-//This is the model block. THe assumption and the learning from my data.
-//It defines my bayesian model 
-//The priors. It is that bfore seing my data, I assume that those are the distribution of my parameters. It is what I believe about a parameter befor seing the data.
-//alpha= is on a log scale. So log(4)~1,4 from the mean of the data. It is the set baseline when elev = 0. First the mean of the normal distribution and then the standard deviation(spread of plausible alpha values)
-//beta is the slope of elevation and we assume always at the beginning that there is no effect of elevation.(Mean = 0 so no effect, and SD, plausible range)
-//phi is the dispersion parameter that allows my variance to be larger than my mean. in a NB, Var(y)= mean (y) + mean(y)^2/phi. MEean = overdispersion around 2 and SD allows phi to vary. and then thrucated at 0 because phi must be positiv
-//theta is the probability of a structural zero. It accounts for the excta 0's. Theta = 0 no structural 0, 1 all 0's are structural. Beta is bounded between 0 an 1. Shape of the parameters is 2,2 so roughly symmetric. THis means that structural zeros are equally likely as sampling zeros. 
 
-//liklihood is basically my model 
-// I loop over each observation in the dataset. and then I need to consider if the 0 is a structural 0 (an observation that is always 0) or a sampling zero (a zero that come from the negative binomial distribution by chance)
-//theta is the mixing properties for structural zeros. and log mix comupter log(p*exp(log_f1) + (1-p)*exp(log_f2)) to see if the zero is a structural or if it comes from the negative binomial
 
+
+//This is to simulated new data from the model and I will use that to do my prior predictive check (before I feed my real y) and the posterior predictive check (after fitting the model)
 generated quantities {
-  array[N] int y_sim;
+  array[N] int y_sim;//a new simulated observation
   for (n in 1:N) {
-    if (bernoulli_rng(theta) == 1)
-      y_sim[n] = 0;
+    //draw from Bernoulli with observation_specfis theta
+    if (bernoulli_rng(theta_n[n]) == 1)
+      y_sim[n] = 0;//structural 0
     else
+    //draws from negative binomial now
       y_sim[n] = neg_binomial_2_log_rng(alpha + beta * elev[n], phi);
   } // for loop
 } // generated quantities
